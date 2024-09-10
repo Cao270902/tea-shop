7d33d648103a0af1a2e4b2cd04365fb8c1d70c5f
nguyen.quoc.dung <nguyen.quoc.dung@B122713-mac.local>
2024-09-10 20:43:45 +0700

    feat: authen

---
+++ src/app/(auth)/login/login-form.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import envConfig from "../../../../config";
+
+const formSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6).max(100),
+  })
+  .strict();
+type FormValues = z.infer<typeof formSchema>;
+
+export default function LoginForm() {
+  console.log(process.env.NEXT_PUBLIC_API_ENDPOINT);
+  // 1. Define your form.
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: FormValues) {
+    // console.log(values);
+    const result = await fetch(
+      `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/auth/login`,
+      {
+        body: JSON.stringify(values),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      }
+    ).then((res) => res.json());
+    // console.log(result);
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-2 max-w-[600px] flex-shrink-0 w-full"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" type="email" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" type="password" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="!mt-8 w-full">
+          Login
+        </Button>
+      </form>
+    </Form>
+  );
+}
